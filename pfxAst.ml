3a3d2a35893fcc710a0259ca0fc77b967f1972a8
s17rivoa <38115475+s17rivoa@users.noreply.github.com>
2018-05-25 21:49:41 +0200

    Create pfxAst.ml

---
+++ pfxAst.ml
@@ -0,0 +1,7 @@
+type command = 
+	| PUSH of int | POP | SWAP | ADD | SUB | MUL | DIV | REM ;;
+
+type program = (* complete me *)
+
+(* add here all useful functions and types  related to the AST: for instance  string_of_ functions *)
+
