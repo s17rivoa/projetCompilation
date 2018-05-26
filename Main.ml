e3aa71d14d4271bd2d2a111f24e678c315c18d9a
s17rivoa <38115475+s17rivoa@users.noreply.github.com>
2018-05-26 10:48:57 +0200

    Update Main.ml

--- Main.ml
+++ Main.ml
@@ -48,6 +48,34 @@ let rec print_list = function
 | e::l -> print_int e ; print_string " " ; print_list l ;;
 
 (*Test*)
-let st = 10::0::3::74::[];; (*stack initielle*)
+(*let st = 10::0::3::74::[];; (*stack initielle*)
 let res_list, l = (step ADD st);;
-print_char '\n'; print_list res_list ; print_char '\n'; print_char '\n';;
+print_char '\n'; print_list res_list ; print_char '\n'; print_char '\n';; *)
+
+type expression =
+  | Const of int
+  | Var of string
+  | Binop of char * expression * expression (*BinOp.t * expression * expression*)
+  | Uminus of expression ;;
+
+let eval env exp = match exp with
+   | Const(c) -> string_of_int c
+   | Var v -> (try string_of_int(List.assoc v env) with Not_found -> failwith ("Unbound variable "^v))
+   ;;
+
+let generate_binop bop = match bop with
+   | '+' -> "ADD"
+   | '-' -> "SUB"
+   | '*' -> "MUL"
+   | '/' -> "DIV"
+   | '%' -> "REM" ;;
+
+let rec generate env expression = match expression with
+   | Const c -> "PUSH"^(eval env expression)
+   | Var v -> "PUSH"^(eval env expression)
+   | Binop(op,e1,e2) -> (generate env e1)^(generate env e2)^(generate_binop op)
+   | Uminus e -> "PUSH0"^"PUSH1"^"SUB"^"PUSH"^(generate env e)^"MUL" ;;
+
+(* test *)
+(*let env = [("coucou",45) ; ("miaou", 78)] in
+print_char '\n' ; print_string(generate env (Binop('+',Var("coucou"),Binop('*',Var "miaou",Const 1))) );; *)
